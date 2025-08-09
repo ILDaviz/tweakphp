94911e7cd389907b05e2e7287811791e095fcb72
David Galet <davidgalet@gmail.com>
2025-08-09 19:39:03 +0200

    refactor: remove redundant copy operation for SQLite build file

--- build.js
+++ build.js
@@ -28,8 +28,6 @@ buildSync({
   ...options,
 })
 
-copySync('node_modules/better-sqlite3/build/Release/better_sqlite3.node', 'build/better_sqlite3.node')
-
 copySync('build/icon.png', 'dist/icon.png')
 copySync('build/icon.icns', 'dist/icon.icns')
 copySync('build/icon.ico', 'dist/icon.ico')
