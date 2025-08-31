fb1d994462745e424af96f8fbd7bc1a30c3dea63
David Galet <davidgalet@gmail.com>
2025-08-31 11:29:46 +0200

    feat: test logic on linux

--- src/main/db/db_manager.ts
+++ src/main/db/db_manager.ts
@@ -4,6 +4,8 @@ import path from 'path'
 const dbPath: string =
   process.env.NODE_ENV === 'development' ? './tweakphp.db' : path.join(process.resourcesPath, './tweakphp.db')
 
+console.log(`DB PATH: ${dbPath}`)
+
 const db = new Database(dbPath)
 db.pragma('journal_mode = WAL')
 
